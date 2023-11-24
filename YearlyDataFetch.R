24fd544af4c004689f49bd70e5cfa1f199d88418
udinh0 <jvnunes145@gmail.com>
2023-11-24 15:38:54 -0300

    Coletar dados diários ao longo de todo o ano, considerando um intervalo de tempo especificado.

--- YearlyDataFetch.R
+++ YearlyDataFetch.R
@@ -1,20 +1,18 @@
-YearlyDataFetch = function(year = c(start_year, end_year)) {
+YearlyDataFetch = function(start_year, end_year) {
   require(lubridate)
   datas = c()
   mes = 1
-  ano_inicial = year[1]
-  ano_final = year[2]
-  while (ano_inicial != ano_final) {
+  while (start_year != end_year) {
     for (dia in 1:31) {
-      datas = append(datas, paste(dia, mes, ano_inicial, sep = "/"))
+      datas = append(datas, paste(dia, mes, start_year, sep = "/"))
     }
     mes = mes + 1
     if (mes == 13) {
       mes = 1
-      ano_inicial = ano_inicial + 1
+      start_year = start_year + 1
     }
   }
-  datas = append(datas, paste(31, 12, ano_inicial, sep = "/"))
+  datas = append(datas, paste(31, 12, start_year, sep = "/"))
   datas = suppressWarnings(dmy(datas))
   datas = datas[!is.na(datas)]
   ultimo = length(datas)
